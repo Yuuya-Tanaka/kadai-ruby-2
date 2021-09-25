1ee7b7cd5d6136c0a9fde0fd57b98d065ef55618
M J <tana12programing@gmail.com>
2021-09-25 13:37:12 +0000

    kadai-fizzbuzz.rb

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,15 +1,15 @@
-num_max = 100
-(1..num_max).each do |num|
-  def fizzbuzz(num)
-    if num % 3 == 0 && num % 5 == 0
-      puts "FizzBuzz"      
-    elsif num % 3 == 0
-      puts "Fizz"
+   def fizzbuzz(num)
+    if (num % 3 == 0) && (num % 5 == 0)
+      return "FizzBuzz"
+    elsif num % 3 == 0 
+      return "Fizz"
     elsif num % 5 == 0
-      puts "Buzz"
+      return "Buzz"
     else
-      puts num
+      return num
     end
-  end  
+   end
+   num_max = 100
+   (1..num_max).each do |num|
   puts fizzbuzz(num)
-end
+  end
